5431686d5bd21606c32271092a6c3227f2b0c82c
Guillaume <glarivie@student.42.fr>
2017-05-01 01:23:04 +0200

    [UPD] Remove react-router-native and add Expo Naviguator

--- App/index.js
+++ App/index.js
@@ -1,46 +1,28 @@
-import React, { Component } from 'react'
-// import { Font } from 'expo'
-import { View } from 'react-native'
+import React from 'react'
 import { Provider } from 'react-redux'
-import { NativeRouter, Route } from 'react-router-native'
-import { Components } from 'exponent'
+import { NavigationProvider, StackNavigation } from '@expo/ex-navigation'
+import { StatusBar } from 'react-native'
 
 import store from './configureStore'
-// import { fonts } from './constants'
+import router from './router'
 
-import Home from './containers/Home'
-import AddNewAccount from './containers/AddNewAccount'
+import colors from './styles/shared/variables.styles'
 
-class App extends Component {
-  state = { ready: false }
-
-  componentWillMount () {
-    this.loadFontsAssets()
-  }
-
-  loadFontsAssets = async () => {
-    // await Promise.all(fonts.map(async font => await Font.loadAsync(font)))
-    this.setState({ ready: true })
-  }
-
-  render () {
-    const { ready } = this.state
-
-    if (!ready)
-      return <Components.AppLoading />
-
-    return (
-      <Provider store={store}>
-        <NativeRouter>
-            <View style={{ flex: 1 }}>
-              <Route exact path="/" component={Home} />
-              <Route path="/account/new" component={AddNewAccount} />
-              {/* <Route path="/topics" component={Topics} /> */}
-            </View>
-        </NativeRouter>
-      </Provider>
-    )
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <NavigationProvider router={router}>
+      <StatusBar barStyle="light-content" />
+      <StackNavigation
+        initialRoute={router.getRoute('Accounts')}
+        defaultRouteConfig={{
+          navigationBar: {
+            backgroundColor: colors.darkblue,
+            tintColor: colors.pistache,
+          }
+        }}
+      />
+    </NavigationProvider>
+  </Provider>
+)
 
 export default App
