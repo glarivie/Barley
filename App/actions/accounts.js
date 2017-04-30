3f480cacd69c6150c0dc239f2f52534a32540aca
Guillaume <glarivie@student.42.fr>
2017-05-01 01:29:00 +0200

    [ADD] deleteAccount and addOperation

--- App/actions/accounts.js
+++ App/actions/accounts.js
@@ -4,6 +4,20 @@ const setAccount = account => async dispatch =>
     data: account,
   })
 
+const deleteAccount = accountID => async dispatch =>
+  dispatch({
+    type: 'DELETE_ACCOUNT',
+    data: accountID,
+  })
+
+const AddNewOperation = (_id, operation) => async dispatch =>
+  dispatch({
+    type: 'ADD_NEW_OPERATION',
+    data: { _id, operation },
+  })
+
 export default {
   setAccount,
-};
+  deleteAccount,
+  AddNewOperation,
+}
--- App/reducers/accounts.js
+++ App/reducers/accounts.js
@@ -2,18 +2,44 @@ import { get } from 'lodash'
 
 import { uniqueID } from '../helpers'
 
-const accountsReducer = (state = {}, action) => {
-  switch (action.type) {
+const accountsReducer = (state = {}, { type, data }) => {
+  switch (type) {
     case 'ADD_NEW_ACCOUNT':
-      return {
+      return ({
         ...state,
         accounts: [
           ...get(state, 'accounts', []), {
-            ...action.data,
+            ...data,
             _id: uniqueID(),
+            data: [],
           },
-        ]
+        ],
+      })
+    case 'ADD_NEW_OPERATION': {
+      const accounts = get(state, 'accounts', [])
+      const _id = accounts.findIndex(({ _id }) => _id === get(data, '_id', 0))
+      const operation = get(data, 'operation', {})
+
+      if (_id !== -1) {
+        accounts[_id].data.unshift({
+          ...operation,
+          _id: uniqueID(),
+        })
+        accounts[_id].amount += operation.amount
       }
+
+      return ({
+        ...state,
+        ...accounts,
+      })
+    }
+    case 'DELETE_ACCOUNT':
+      return ({
+        ...state,
+        accounts: [
+          ...get(state, 'accounts', []).filter(({ _id }) => _id !== data)
+        ],
+      })
     default:
       return state
   }
