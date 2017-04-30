7e7aec88c21e73a103ddc299be22ffffb7b71d01
Guillaume <glarivie@student.42.fr>
2017-05-01 01:24:27 +0200

    [UPD] default colors

--- App/styles/shared/variables.styles.js
+++ App/styles/shared/variables.styles.js
@@ -1,9 +1,20 @@
-const white = '#FFFFFF'
 const lightgrey = '#EEEEEE'
 const darkgrey = '#BBBBBB'
+const darkblue = '#193441'
+const lightblue = '#3E606F'
+const pistache = '#91AA9D'
+const white = '#FCFFF5'
+const beige = '#D1DBBD'
 
-export {
-  white,
+const error = '#a23c3c'
+
+export default {
   lightgrey,
   darkgrey,
+  darkblue,
+  lightblue,
+  pistache,
+  white,
+  beige,
+  error,
 }
