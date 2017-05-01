89b070d29b66c1d465d466e421c5f23ebc463141
Guillaume <glarivie@student.42.fr>
2017-05-01 23:23:56 +0200

    [ADD] Components to router

--- App/router.js
+++ App/router.js
@@ -4,12 +4,14 @@ import Accounts from './containers/Accounts'
 import AddNewAccount from './containers/AddNewAccount'
 import AddNewOperation from './containers/AddNewOperation'
 import AccountSingle from './containers/AccountSingle'
+import EditAccount from './containers/EditAccount'
 
 const router = createRouter(() => ({
   Accounts: () => Accounts,
   AddNewAccount: () => AddNewAccount,
   AddNewOperation: () => AddNewOperation,
   AccountSingle: () => AccountSingle,
+  EditAccount: () => EditAccount,
 }))
 
 export default router
