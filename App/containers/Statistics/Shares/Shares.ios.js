4fbb29765cffd4fc578422755b3c9b84e4dcf8a1
Guillaume <glarivie@student.42.fr>
2017-05-13 11:45:21 +0200

    [UPD] Donuts instead of Pie

--- App/containers/Statistics/Shares/Shares.ios.js
+++ App/containers/Statistics/Shares/Shares.ios.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { View, Text, Dimensions } from 'react-native'
 import PieChart from 'react-native-pie-chart'
 import _ from 'lodash'
+import R from 'ramda'
 
 import statsColors from '../../../styles/shared/statsColors.styles'
 import styles from './Shares.styles'
@@ -10,14 +11,18 @@ import styles from './Shares.styles'
 const Shares = ({ data }) => {
   const stats = _(data)
     .groupBy('category')
-    .reduce((acc, operation, category) =>
-      _.set(acc, [category], _.sum(operation.map(({ amount }) => amount)))
-    , {})
+    .map((operation, category) => ({
+      category,
+      amount: R.sum(R.pluck('amount', operation)),
+    }))
+    .sortBy('amount')
+    .reverse()
+    .value()
 
-  const series = _.map(stats, amount => amount)
-  const sliceColors = Object.keys(stats).map(category => statsColors[category])
-  const total = _.sum(series)
-  const shares = _.map(stats, amount => amount / total * 100 | 0)
+  const series = R.pluck('amount', stats)
+  const sliceColors = stats.map(({ category }) => statsColors[category])
+  const total = R.sum(series)
+  const shares = stats.map(({ amount }) => amount / total * 100 | 0)
 
   return (
     <View style={styles.Shares}>
@@ -25,13 +30,16 @@ const Shares = ({ data }) => {
         chart_wh={Dimensions.get('window').width - 80}
         series={series}
         sliceColor={sliceColors}
+        coverRadius={0.45}
+        coverFill={'#FFF'}
+        doughnut
       />
       <View style={styles.legend}>
-        {Object.entries(stats).map(([category, amount], index) => (
+        {stats.map(({ category, amount }, index) => (
           <View style={styles.row} key={index}>
             <View style={styles[category]} />
             <View style={styles.category}>
-              <Text style={styles.left}>{category} : {amount} € ({shares[index]} %)</Text>
+              <Text style={styles.left}>{category} : {amount.toFixed(2)} € ({shares[index]} %)</Text>
             </View>
           </View>
         ))}
