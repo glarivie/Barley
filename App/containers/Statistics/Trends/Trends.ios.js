19f3350649f6b32c4fcf33f43e169546ea116ff2
Guillaume L <guillaume@misterfox.co>
2017-05-17 11:19:55 +0200

    [UPD] add date to last spent

--- App/containers/Statistics/Trends/Trends.ios.js
+++ App/containers/Statistics/Trends/Trends.ios.js
@@ -9,13 +9,12 @@ import styles from './Trends.styles'
 const Trends = ({ data, available }) => {
   const debitByDay = _(data)
     .groupBy('date')
-    .mapValues(value => _.sum(value.map(({ amount }) => -amount)))
-    .values()
+    .map((value, date) => ({ date, amount: _.sum(value.map(({ amount }) => -amount)) }))
     .value()
 
   const meanByDay = _(getDate(new Date()))
     .range()
-    .map(index => _.get(debitByDay, [index], 0))
+    .map(index => _.get(debitByDay, `[${index}].amount`, 0))
     .sort((a, b) => a - b)
     .slice(1, -1) // Remove min and max
     .mean()
@@ -33,8 +32,8 @@ const Trends = ({ data, available }) => {
       <Text style={styles.margin}>Total available</Text>
       <Text style={styles.big}>{meanByDay.toFixed(2)} €</Text>
       <Text style={styles.margin}>Spent by day (mean)</Text>
-      <Text style={styles.big}>{debitByDay[0].toFixed(2)} €</Text>
-      <Text style={styles.margin}>Spent today</Text>
+      <Text style={styles.big}>{debitByDay[0].amount.toFixed(2)} €</Text>
+      <Text style={styles.margin}>Spent on {debitByDay[0].date}</Text>
       <Text style={styles.big}>{dayLeftBeforeBankruptcy}</Text>
       <Text style={styles.color}>Day(s) left before bankruptcy</Text>
       <Text style={styles.color}>{daysLeftInMonth} day(s) left in month</Text>
