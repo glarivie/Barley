f8bf1ca55f35d67cd1549f0a0072848cd9e6b569
Guillaume L <guillaume@misterfox.co>
2017-08-21 16:30:41 +0200

    [ADD] colors variables

--- src/styles/shared/variables.styles.js
+++ src/styles/shared/variables.styles.js
@@ -1,5 +1,11 @@
 const colors = {
-  white: '#FFFFFF',
+  $white: '#FCFFF5',
+  $blue: '#40A8BC',
+  $keppel: '#3592A5',
+  $topaz: '#FFC56E',
+  $orange: '#F46C4E',
+  $apricot: '#FBBA72',
+  $grey: '#404E4D',
 }
 
 export {
